e525bda825ad1c28bdb107198689144db83397f0
z2955333032 <295533032@qq.com>
2022-05-16 22:24:07 +0800

    Update config.py

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ DEBUG = True
 # 读取数据库环境变量
 username = os.environ.get("MYSQL_USERNAME", 'root')
 password = os.environ.get("MYSQL_PASSWORD", 'bST3Njt6')
-db_address = os.environ.get("MYSQL_ADDRESS", '127.0.0.1:3306')
+db_address = os.environ.get("MYSQL_ADDRESS", '10.0.224.16:3306')
