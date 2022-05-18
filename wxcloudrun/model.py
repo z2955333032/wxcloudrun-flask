7116c4ba45eeb3d6cc3e32810e6a070958ecc4aa
z2955333032 <295533032@qq.com>
2022-05-18 15:10:25 +0800

    Update model.py

--- wxcloudrun/model.py
+++ wxcloudrun/model.py
@@ -14,7 +14,14 @@ class Counters(db.Model):
     created_at = db.Column('createdAt', db.TIMESTAMP, nullable=False, default=datetime.now())
     updated_at = db.Column('updatedAt', db.TIMESTAMP, nullable=False, default=datetime.now())
     
-    
+class JDF33(db.Model):
+    # 设置结构体表格名称
+    __tablename__ = 'JDF33'
+
+    # 设定结构体对应表格的字段
+    id = db.Column(db.Integer, primary_key=True)
+    xm = db.Column(db.String(12))   
+    sfzhm = db.Column(db.String(99))
 class JDF(db.Model):
     # 设置结构体表格名称
     __tablename__ = 'JDF'
