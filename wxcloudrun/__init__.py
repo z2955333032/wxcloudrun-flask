6a07cc11e6f2ad0aa2ef2297f9e0330d28f8ec95
z2955333032 <295533032@qq.com>
2022-05-18 15:36:52 +0800

    Update __init__.py

--- wxcloudrun/__init__.py
+++ wxcloudrun/__init__.py
@@ -14,6 +14,8 @@ app.config['DEBUG'] = config.DEBUG
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://{}:{}@{}/flask_demo'.format(config.username, config.password,
                                                                              config.db_address)
 
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False 
+
 # 初始化DB操作对象
 db = SQLAlchemy(app)
 
